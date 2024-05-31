fdbeac16fcbd298ffa470743e60df28ac9fcccfd
juancig01 <juanchoinsuasty@gmail.com>
2024-05-31 10:05:24 -0500

    poderes

--- src/main/java/com/eebp/accionistas/backend/asamblea/repositories/PoderRepository.java
+++ src/main/java/com/eebp/accionistas/backend/asamblea/repositories/PoderRepository.java
@@ -55,18 +55,18 @@ public interface PoderRepository extends JpaRepository<Poder, Integer> {
 
     @Query(value = "SELECT ra.id_asistente, ra.huella, ra.asistencia, ra.ide_Per, " +
             "CONCAT(apoderado.nomPri, ' ', apoderado.nomSeg, ' ', apoderado.apePri, ' ', apoderado.apeSeg) AS nombreApoderado, " +
-            "p.id_Poderdante, " +
+            "p.id_poderdante, " +
             "CONCAT(poderdante.nomPri, ' ', poderdante.nomSeg, ' ', poderdante.apePri, ' ', poderdante.apeSeg) AS nombrePoderdante, " +
             "CASE WHEN a1.tipoAccionista IN (1, 2) OR a2.tipoAccionista IN (1, 2) THEN 'SI' ELSE 'NO' END AS validacion " +
             "FROM registro_asamblea ra " +
-            "LEFT JOIN (SELECT p1.* FROM Poder p1 " +
-            "INNER JOIN (SELECT id_apoderado, MAX(consecutivo) AS max_consecutivo FROM Poder GROUP BY id_apoderado) p2 " +
+            "LEFT JOIN (SELECT p1.* FROM poder p1 " +
+            "INNER JOIN (SELECT id_apoderado, MAX(consecutivo) AS max_consecutivo FROM poder GROUP BY id_apoderado) p2 " +
             "ON p1.id_apoderado = p2.id_apoderado AND p1.consecutivo = p2.max_consecutivo) p ON ra.ide_per = p.id_apoderado " +
-            "LEFT JOIN Persona apoderado ON ra.ide_per = apoderado.COD_USUARIO " +
-            "LEFT JOIN Persona poderdante ON p.id_Poderdante = poderdante.COD_USUARIO " +
-            "LEFT JOIN Accionista a1 ON apoderado.COD_USUARIO = a1.COD_USUARIO " +
-            "LEFT JOIN Accionista a2 ON poderdante.COD_USUARIO = a2.COD_USUARIO " +
-            "WHERE ra.consecutivo = (SELECT MAX(r.consecutivo) FROM Registro_Asamblea r) " +
-            "AND ra.ide_Per = :idePer", nativeQuery = true)
+            "LEFT JOIN persona apoderado ON ra.ide_per = apoderado.COD_USUARIO " +
+            "LEFT JOIN persona poderdante ON p.id_poderdante = poderdante.COD_USUARIO " +
+            "LEFT JOIN accionista a1 ON apoderado.COD_USUARIO = a1.COD_USUARIO " +
+            "LEFT JOIN accionista a2 ON poderdante.COD_USUARIO = a2.COD_USUARIO " +
+            "WHERE ra.consecutivo = (SELECT MAX(r.consecutivo) FROM registro_asamblea r) " +
+            "AND ra.ide_per = :idePer", nativeQuery = true)
     List<Object[]> obtenerDetallesPorIdePer(@Param("idePer") String idePer);
 }
